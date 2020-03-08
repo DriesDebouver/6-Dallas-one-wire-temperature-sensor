d6c7b88cf29f44f50ab6359e2867ca7c08e11945
DriesDebouver <driesdebouver@hotmail.com>
2020-03-08 16:52:36 +0100

    Update DallasTempSensors.ino

--- DallasTempSensors.ino
+++ DallasTempSensors.ino
@@ -5,10 +5,6 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 #include <Adafruit_ADS1015.h>
-#define BLYNK_PRINT Serial
-#include <WiFi.h>
-#include <WiFiClient.h>
-#include <BlynkSimpleEsp32.h>
 
 //OLED:
 Adafruit_SSD1306 display(128, 64, &Wire, -1);
@@ -24,13 +20,6 @@ DallasTemperature sensors(&oneWire);
 DeviceAddress sensor1 = {  };
 DeviceAddress sensor2 = {  };
 float tempCEen;
-float tempCTwee;
-
-//WiFi:
-int pin = 19;
-char auth[] = "";
-char ssid[] = "";
-char pass[] = "";
 
 void setup() {
   pinMode(pin, OUTPUT);
@@ -44,22 +33,9 @@ void setup() {
     Serial.println(F("SSD1306 allocation failed"));
     for (;;);
   }
-
-  //WiFi:
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
-  WiFi.begin(ssid, pass);
-  int wifi_ctr = 0;
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(250);
-    Serial.print(".");
-  }
-  Serial.println("WiFi connected");
-  Blynk.begin(auth, ssid, pass);
 }
 
 void loop() {
-  Blynk.run();
   sensors.requestTemperatures();
   tempCEen = sensors.getTempC(sensor1);
   tempCTwee = sensors.getTempC(sensor2);
@@ -67,9 +43,6 @@ void loop() {
   adc = ads.readADC_SingleEnded(0);
   temp = (adc * 0.1875) / 1000;
   temp1 = temp * 10;
-  Blynk.virtualWrite(V1, tempCEen);
-  Blynk.virtualWrite(V2, tempCTwee);
-  Blynk.virtualWrite(V3, temp1);
 
   //Serial monitor:
   Serial.print("Sensor 1 (°C): ");
@@ -99,5 +72,6 @@ void loop() {
   display.print((char)247); // ' ° ' symbol
   display.print("C");
   display.display();
+  
   delay(1000);
 }
